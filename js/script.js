e1278c64f57fc6d9c88d9e0f8bb60aa8d1ff8daf
darkdigger011 <darkdigger011@gmail.com>
2017-08-13 22:12:32 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@
 	}
 
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET","/feed.xml");
+	xmlhttp.open("GET","/sitemap.xml");
 	xmlhttp.onreadystatechange = function () {
 		if (xmlhttp.readyState != 4) return;
 		if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
